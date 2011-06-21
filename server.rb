028c2a50837548ebb2e92d7bbf7e24be5d3eadb3
Jeff Remer <jeff@threestarchina.com>
2011-06-21 13:40:20 -0700

    Basic parameter checking for the /sms hook
    Validates Twilio requests have the right information and format

--- server.rb
+++ server.rb
@@ -7,3 +7,25 @@ require 'sinatra'
 get '/' do
   "Hello World"
 end
+
+post '/sms' do
+  if valid_params? params
+    status 200
+  else
+    status 400
+  end
+end 
+
+def valid_params?(params)
+  required_params = {
+    :SmsSid => /\w{34}/,
+    :AccountSid => /\w{34}/,
+    :From => /^(\+1|1)?([2-9]\d\d[2-9]\d{6})$/,
+    :To => /^(\+1|1)?([2-9]\d\d[2-9]\d{6})$/,
+    :Body => /\w+/
+  }
+  required_params.each do |key, value|
+      return false unless params[key]
+      return false unless params[key] =~ required_params[key]
+  end
+end
--- spec/sfmuni_spec.rb
+++ spec/sfmuni_spec.rb
@@ -6,6 +6,17 @@ describe "SFMuni" do
   def app
     @app ||= Sinatra::Application
   end
+
+  before(:each) do 
+    @sms_params = {
+      :SmsSid => (0...34).map{ ('a'..'z').to_a[rand(26)] }.join,
+      :AccountSid => (0...34).map{ ('a'..'z').to_a[rand(26)] }.join,
+      :From => '4155554567',
+      :To => '4155554567',
+      :Body => '21 inbound hayes and shrader'
+    }
+  end
+
   
   it "should respond to /" do
     get '/'
@@ -13,7 +24,15 @@ describe "SFMuni" do
   end
   
   describe "/sms" do
-    pending "should respond ok with a valid Twilio request"
-    pending "should respond bad requst with an invalid Twilio request"
+    it "should respond ok with a valid Twilio request" do
+      post('/sms', @sms_params)
+      last_response.should be_ok
+      last_response.status.should == 200
+    end
+    it "should respond bad requst with an invalid Twilio request" do
+      post('/sms', @sms_params.merge!({:Body => ''}))
+      last_response.should_not be_successful
+      last_response.status.should == 400      
+    end
   end
 end
