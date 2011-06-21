a35a214cb05386fcedd916868cd39593965000f4
Jeff Remer <jeff@threestarchina.com>
2011-06-20 18:15:17 -0700

    Adding some pending specs to implement around /sms

--- spec/sfmuni_spec.rb
+++ spec/sfmuni_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "My App" do
+describe "SFMuni" do
   include Rack::Test::Methods
   
   def app
@@ -10,5 +10,10 @@ describe "My App" do
   it "should respond to /" do
     get '/'
     last_response.should be_ok
+  end
+  
+  describe "/sms" do
+    pending "should respond ok with a valid Twilio request"
+    pending "should respond bad requst with an invalid Twilio request"
   end
 end
